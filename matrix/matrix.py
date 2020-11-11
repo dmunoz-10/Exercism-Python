ddbc63812b4af70ddb2d9ad499e4383057c2d944
Daniel Muñoz <djmv225@gmail.com>
2020-11-11 00:20:01 -0500

    Darts exercise improved

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,10 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix = [i.split(' ') for i in matrix_string.split('\n')]
+        self.matrix = [list(map(int, row.split()))
+                       for row in matrix_string.splitlines()]
 
     def row(self, index):
-        return [int(i) for i in self.matrix[index - 1]]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        return [int(i[index - 1]) for i in self.matrix]
+        return [i[index - 1] for i in self.matrix]
